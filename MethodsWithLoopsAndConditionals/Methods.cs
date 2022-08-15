47c3c6c1438da8743f6df78210262bbe2944f269
Jennie Duffer <jenniej297@gmail.com>
2022-08-15 09:29:47 -0500

    initial commit

---
+++ MethodsWithLoopsAndConditionals/Methods.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Methods
+    {
+        //public static void PrintThousand()
+        //{
+        //    for (int i = 1000; i >= -1000; i--)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+        //public static void ByThree()
+        //{
+        //    for (int i = 3; i <= 99; i += 3)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //}
+
+        //public static void EqualOrNot(int a, int b)
+        //{
+        //    if (a == b)
+        //    {
+        //        Console.WriteLine("these numbers are equal");
+
+        //    }
+        //    else
+        //        Console.WriteLine("these numbers are not equal");
+
+        //}
+
+        //        public static void EvenOdd(int a)
+        //        {
+        //            if (a % 2 ==0 )
+        //            {
+        //                Console.WriteLine($"{a} is even");
+        //            }
+        //            else
+        //            {
+        //                Console.WriteLine($"{a} is odd");
+        //;           }
+        //        }
+
+        //public static void NegPos(int a)
+        //{
+
+        //    Console.WriteLine("Pick a number: ");
+        //    a = int.Parse(Console.ReadLine());
+
+        //    if (a == 0)
+        //    {
+        //        Console.WriteLine("{a} is zero");
+        //    }
+        //    else if (a > 0)
+        //    {
+        //        Console.WriteLine("{a} is positive");
+        //    }
+        //    else 
+        //    {
+        //        Console.WriteLine("{a} is negative");
+        //    }
+           
+
+        //}
+
+        //public static void VoterAge(int a)
+        //{
+        //    Console.WriteLine("How old are you?");
+        //    a = int.Parse(Console.ReadLine());
+
+        //    if (a >= 18)
+        //    {
+        //        Console.WriteLine("Yes, you can vote!");
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("No, you can not vote!");
+        //    }
+        //}
+
+        //public static void RangeOfTen(int i)
+        //{
+        //    Console.WriteLine("Pick a number between -10 and 10. ");
+        //    i = int.Parse(Console.ReadLine());
+
+
+        //    if (i >= 0)
+        //    {
+        //        Console.WriteLine("your number is within range");
+        //    }
+        //    else
+            
+        //    {
+        //        Console.WriteLine("your number is not within range");
+        //    }
+        //}
+
+        //public static void MultiplicationTable()
+        //{
+        //    Console.WriteLine("Enter the number: ");
+        //    int number = Convert.ToInt32(Console.ReadLine());
+
+        //    for (int i = 1; i <= 12; i++)
+        //    {
+        //        Console.WriteLine($"{i} x {number} = {number * i}");
+        //    }
+
+        //}
+
+
+
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,18 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+
+//Methods.PrintThousand();
+
+//Methods.ByThree();
+
+//Methods.EqualOrNot(4, 6);
+
+//Methods.EvenOdd(7);
+
+//Methods.NegPos(-5);
+
+//Methods.VoterAge(19);
+
+//Methods.RangeOfTen(6);
+
+Methods.MultiplicationTable();
